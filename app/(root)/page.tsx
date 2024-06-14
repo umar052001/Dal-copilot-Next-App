ea5ff6807a1f55a29f30c693c62544d6b96e8484
Khawar Sultan <khawarsultan.developer@gmail.com>
2024-06-14 19:00:53 +0500

    changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 // app/page.js
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import HomeClient from "./homeClient";
+import HomeClient from "./HomeClient";
 
 export default async function HomePage() {
   const { userId } = auth();
