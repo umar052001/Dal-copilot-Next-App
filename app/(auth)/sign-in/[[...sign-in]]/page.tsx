37144e1bd679806c8b2cc698641967687c4630a0
Khawar Sultan <khawarsultan.developer@gmail.com>
2024-06-16 14:16:20 +0500

    cleark custom signin/signup

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,73 @@
-import { SignIn } from '@clerk/nextjs'
-import React from 'react'
+'use client';
 
-const page = () => {
+import * as Clerk from '@clerk/elements/common';
+import * as SignIn from '@clerk/elements/sign-in';
+import Image from "next/image";
+import Link from 'next/link';
+
+export default function SignInPage() {
   return (
-    <SignIn/>
-  )
-}
+    <div className="flex-center w-full flex-grow items-center  px-4 sm:justify-center">
+      <SignIn.Root>
+        <SignIn.Step
+          name="start"
+          className="lg:w-[32%] md:w-[50%] w-full space-y-6 rounded-2xl  px-4 py-10 sm:px-8"
+        >
+          <header className="text-center flex flex-col items-center">
+            <Image src="https://www.dal-demo.live/static/media/LogoMark.b58bee8dcba820ab1e4cfb4edb402eb3.svg"
+              alt="search"
+              width={75}
+              height={75}
+            />
 
-export default page
+            <h1 className="mt-4 text-2xl tracking-tight text-zinc-950 font-bold">Login to your account</h1>
+            <h1 className="mt-3 text-sm tracking-tight text-zinc-950 ">Please create an account to test the application</h1>
+            <p className='arabicsfont'>الرجاء خلق حساب لتجربة الخدمة</p>
+            <h1 className="mt-3 text-sm tracking-tight text-zinc-950">
+              <b>Note:</b> Our server, powered by a high-cost GPU, operates on-demand for efficiency. Please email{' '}
+              <a href="mailto:saltamimi@modernint.sato" className="text-[#00d9dd]  ">
+                saltamimi@modernint.sato
+              </a>{' '}
+              to activate it for your testing needs.
+            </h1>
+          </header>
+          <Clerk.GlobalError className="block text-sm text-red-400" />
+          <div className="space-y-4">
+            <Clerk.Field name="identifier" className="space-y-2">
+              <Clerk.Label className="text-sm font-medium text-zinc-950">Username</Clerk.Label>
+              <Clerk.Input
+                type="text"
+                required
+                className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
+              />
+              <Clerk.FieldError className="block text-sm text-red-400" />
+            </Clerk.Field>
+            <Clerk.Field name="password" className="space-y-2">
+              <Clerk.Label className="text-sm  font-medium text-zinc-950">Password</Clerk.Label>
+              <Clerk.Input
+                type="password"
+                required
+                className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
+              />
+              <Clerk.FieldError className="block text-sm text-red-400" />
+            </Clerk.Field>
+          </div>
+          <SignIn.Action
+            submit
+            className="w-full rounded-md bg-[#00d9dd] px-3.5 py-1.5 text-center text-sm font-medium text-white shadow outline-none ring-1 ring-inset ring-[#00d9dd] hover:bg-[#00d9dd] focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-[#00d9dd] active:text-white/70"
+          >
+            Sign In
+          </SignIn.Action>
+
+
+          <p className="text-center text-sm text-zinc-500">
+            Don&apos;t have an account yet?{' '}
+            <Link href="/sign-up" className="font-medium text-[#00d9dd] decoration-[#00d9dd] underline-offset-4 outline-none hover:text-[#00d9dd] hover:underline focus-visible:underline">
+              Sign up
+            </Link>
+          </p>
+        </SignIn.Step>
+      </SignIn.Root>
+    </div>
+  );
+}
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,137 @@
-import {  SignUp } from '@clerk/nextjs'
-import React from 'react'
+'use client';
+import Link from 'next/link';
 
-const page = () => {
+import * as Clerk from '@clerk/elements/common';
+import * as SignUp from '@clerk/elements/sign-up';
+import Image from "next/image";
+
+export default function SignUpPage() {
   return (
-    <SignUp/>
-  )
-}
+    <div className="flex-center w-full flex-grow items-center  px-4 sm:justify-center">
+      <SignUp.Root>
+        <SignUp.Step
+          name="start"
+          // shadow-md ring-1 ring-black /5 
+          className="lg:w-[32%] md:w-[50%] w-full space-y-6 rounded-2xl  px-4 py-10 sm:px-8"
+        >
+          <header className="text-center flex flex-col items-center ">
+            <Image src="https://www.dal-demo.live/static/media/LogoMark.b58bee8dcba820ab1e4cfb4edb402eb3.svg"
+              alt="search"
+              width={75}
+              height={75}
+            />
 
-export default page
+            <h1 className="mt-4 text-xl tracking-tight text-zinc-950 font-bold">Signup to create an account</h1>
+          </header>
+          <Clerk.GlobalError className="block text-sm text-red-400" />
+          <div className="space-y-4">
+            <Clerk.Field name="emailAddress" className="space-y-2">
+              <Clerk.Label className="text-sm font-medium text-zinc-950">Email</Clerk.Label>
+              <Clerk.Input
+                type="email"
+                required
+                className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
+              />
+              <Clerk.FieldError className="block text-sm text-red-400" />
+            </Clerk.Field>
+            <Clerk.Field name="password" className="space-y-2">
+              <Clerk.Label className="text-sm font-medium text-zinc-950">Password</Clerk.Label>
+              <Clerk.Input
+                type="password"
+                required
+                className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
+              />
+              <Clerk.FieldError className="block text-sm text-red-400" />
+            </Clerk.Field>
+          </div>
+          <SignUp.Action
+            submit
+            className="w-full rounded-md bg-[#00d9dd] px-3.5 py-1.5 text-center text-sm font-medium text-white shadow outline-none ring-1 ring-inset ring-[#00d9dd] hover:bg-[#00d9dd] focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-[#00d9dd] active:text-white/70"
+          >
+            Sign Up
+          </SignUp.Action>
+
+          <p className="text-center text-sm text-zinc-500">
+            Already have an account?{' '}
+            <Link href="/sign-in" className="font-medium text-[#00d9dd] decoration-[#00d9dd] underline-offset-4 outline-none hover:text-[#00d9dd] hover:underline focus-visible:underline">
+                Sign in
+            </Link>
+          </p>
+        </SignUp.Step>
+        <SignUp.Step
+          name="verifications"
+          className="lg:w-[32%] md:w-[50%] w-full space-y-6 rounded-2xl  px-4 py-10 sm:px-8"
+        >
+          <header className="text-center flex flex-col items-center ">
+            <Image src="https://www.dal-demo.live/static/media/LogoMark.b58bee8dcba820ab1e4cfb4edb402eb3.svg"
+              alt="search"
+              width={75}
+              height={75}
+            />
+            <h1 className="mt-4 text-xl  tracking-tight text-zinc-950 font-bold">Verify email code</h1>
+          </header>
+          <Clerk.GlobalError className="block text-sm text-red-400" />
+          <SignUp.Strategy name="email_code">
+            <Clerk.Field name="code" className="space-y-2">
+              <Clerk.Label className="text-sm font-medium text-zinc-950">Email code</Clerk.Label>
+              <Clerk.Input
+                type="otp"
+                required
+                className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
+              />
+              <Clerk.FieldError className="block text-sm text-red-400" />
+            </Clerk.Field>
+            <SignUp.Action
+              submit
+              className="w-full rounded-md bg-[#00d9dd] px-3.5 py-1.5 text-center text-sm font-medium text-white shadow outline-none ring-1 ring-inset ring-[#00d9dd] hover:bg-[#28c0c3] focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-[#28c0c3] active:text-white/70"
+            >
+              Verify
+            </SignUp.Action>
+          </SignUp.Strategy>
+          
+          <p className="text-center text-sm text-zinc-500">
+            Already have an account?{' '}
+            <Link href="/sign-in" className="font-medium text-[#00d9dd] decoration-[#00d9dd] underline-offset-4 outline-none hover:text-[#00d9dd] hover:underline focus-visible:underline">
+              Sign in
+            </Link>
+          </p>
+        </SignUp.Step>
+        <SignUp.Step
+          name="continue"
+          className="lg:w-[32%] md:w-[50%] w-full space-y-6 rounded-2xl  px-4 py-10 sm:px-8"
+        >
+          <header className="text-center flex flex-col items-center">
+            <Image src="https://www.dal-demo.live/static/media/LogoMark.b58bee8dcba820ab1e4cfb4edb402eb3.svg"
+              alt="search"
+              width={75}
+              height={75}
+            />
+            <h1 className="mt-4 text-xl  tracking-tight text-zinc-950 font-bold">Continue registration</h1>
+          </header>
+          <Clerk.GlobalError className="block text-sm text-red-400" />
+          <Clerk.Field name="username" className="space-y-2">
+            <Clerk.Label className="text-sm font-medium text-zinc-950">Username</Clerk.Label>
+            <Clerk.Input
+              type="text"
+              required
+              className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
+            />
+            <Clerk.FieldError className="block text-sm text-red-400" />
+          </Clerk.Field>
+          <SignUp.Action
+            submit
+            className="w-full rounded-md bg-[#00d9dd] px-3.5 py-1.5 text-center text-sm font-medium text-white shadow outline-none ring-1 ring-inset ring-[#00d9dd] hover:bg-[#00d9dd] focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-[#00d9dd] active:text-white/70"
+          >
+            Continue
+          </SignUp.Action>
+          <p className="text-center text-sm text-zinc-500">
+            Already have an account?{' '}
+            <Link href="/sign-in" className="font-medium text-[#00d9dd] decoration-[#00d9dd] underline-offset-4 outline-none hover:text-[#00d9dd] hover:underline focus-visible:underline">
+              Sign in
+            </Link>
+          </p>
+        </SignUp.Step>
+      </SignUp.Root>
+    </div>
+  );
+}
