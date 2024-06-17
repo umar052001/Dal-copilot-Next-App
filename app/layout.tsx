531695b615755fedd1c3fdb4b3e30884fea41468
Khawar Sultan <khawarsultan.developer@gmail.com>
2024-06-17 23:23:18 +0500

    completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="overflow-y-hidden" >
+      <body >
         <ClerkProvider>
           <LanguageProvider>
             <SidebarProvider>
